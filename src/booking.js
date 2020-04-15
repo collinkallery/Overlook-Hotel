f6f7ddd0319fd771f6c5265493d054e770a74992
collinkallery <collin.kallery@colorado.edu>
2020-04-15 15:51:48 -0600

    :crystal_ball: Build out initial structure of five classes

---
+++ src/booking.js
@@ -0,0 +1,11 @@
+class Booking {
+  constructor() {
+    this.id = bookingId;
+    this.userId = userId;
+    this.date = date;
+    this.roomNumber = roomNumber;
+    this.roomServiceCharges = roomServiceCharges;
+  }
+}
+
+export default Booking;
---
+++ src/manager.js
@@ -0,0 +1,21 @@
+class Manager {
+  constructor() {
+    this.username = 'manager';
+    this.password = 'overlook2019';
+  }
+  createBooking() {
+    // method that allows manager to
+    // create booking for a customer
+  }
+  deleteBooking() {
+    // method that allows manager to
+    // delete booking for a customer
+  }
+  searchByCustomerName() {
+    // method that allows manager to
+    // search through all users by their
+    // name and filter out others
+  }
+}
+
+export default Manager;
