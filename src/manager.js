ebe3414539bfe396738f08c4a9302de56e39a992
collinkallery <collin.kallery@colorado.edu>
2020-04-21 22:23:15 -0600

    :crystal_ball: Add in delete request

--- src/manager.js
+++ src/manager.js
@@ -26,9 +26,20 @@ class Manager {
       });
     domUpdates.displayManagerReservationConfirmation(user, date, roomNumber);
   };
-  deleteBooking() {
-    console.log('deleteReached')
-  }
+  deleteBooking(bookingId) {
+    console.log('hi');
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
+       method: 'DELETE',
+       headers: {
+         'Content-Type': 'application/json'
+       },
+       body: JSON.stringify({
+         id: bookingId
+       })
+     })
+     .then(response => console.log(response))
+     .catch(err => console.log(err));
+   };
   searchByCustomerName() {
     // method that allows manager to
     // search through all users by their
